803cec236c9d1374294b7272ff712d841a65a4a3
MdObaidullah629 <hujaifaobaidullah629@gmail.com>
2026-04-16 15:55:50 +0600

    error page design

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,21 +1,21 @@
-// import Link from "next/link";
+import Link from "next/link";
 
-// export default function NotFound() {
-//   return (
-//     <div className="h-screen flex flex-col items-center justify-center text-center px-10">
-//       <h1 className="text-9xl font-extrabold text-[#1E3A2F] opacity-20">404</h1>
-//       <div className="absolute">
-//         <h2 className="text-3xl font-bold text-gray-800 mb-4">Oops! Page Not Found</h2>
-//         <p className="text-gray-500 mb-8 max-w-md mx-auto">
-//           The page you are looking for might have been removed or is temporarily unavailable.
-//         </p>
-//         <Link 
-//           href="/" 
-//           className="bg-[#1E3A2F] text-white px-8 py-3 rounded-full font-medium hover:bg-opacity-90 transition shadow-lg"
-//         >
-//           Back to Home
-//         </Link>
-//       </div>
-//     </div>
-//   );
-// }
+export default function NotFound() {
+  return (
+    <div className="h-screen flex flex-col items-center justify-center text-center px-10">
+      <h1 className="text-9xl font-extrabold text-[#1E3A2F] opacity-20">404</h1>
+      <div className="absolute">
+        <h2 className="text-3xl font-bold text-gray-800 mb-4">Oops! Page Not Found</h2>
+        <p className="text-gray-500 mb-8 max-w-md mx-auto">
+          The page you are looking for might have been removed or is temporarily unavailable.
+        </p>
+        <Link 
+          href="/" 
+          className="bg-[#1E3A2F] text-white px-8 py-3 rounded-full font-medium hover:bg-opacity-90 transition shadow-lg"
+        >
+          Back to Home
+        </Link>
+      </div>
+    </div>
+  );
+}
